9373571563c70bff8642cad16c0b34a36b74068b
niko.brusselaers <niko.brusselaers@student.ehb.be>
2021-12-14 11:41:39 +0100

    push latest version

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /*
 used sources:
 
@@ -8,7 +7,9 @@ used sources:
 
 const express = require('express')
 const cors = require('cors')
-const {MongoClient} = require('mongodb')
+const {
+    MongoClient
+} = require('mongodb')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -18,7 +19,10 @@ app.use(cors())
 app.use(bodyParser.json())
 
 const uri = "mongodb+srv://courseProjectRestApi:ThisIsThePassword@cluster0.ruiua.mongodb.net/Courseproject?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 
 //temp
@@ -32,11 +36,11 @@ app.get('/', async (req, res) => {
 
 app.post('/create-account', async (req, res) => {
 
-    if ( !req.body.username || !req.body.password || !req.body.email || !req.body.firstname || !req.body.lastname ) {
+    if (!req.body.username || !req.body.password || !req.body.email || !req.body.firstname || !req.body.lastname) {
         res.status(400).send("please fill everything in");
         return
     }
-    
+
     try {
         await client.connect();
         const data = client.db().collection('user_data');
@@ -48,12 +52,16 @@ app.post('/create-account', async (req, res) => {
             email: req.body.email
         })
         console.log(checkusername);
-        if (checkusername){
-            res.status(409).send({error: "the username is already taken"});
+        if (checkusername) {
+            res.status(409).send({
+                error: "the username is already taken"
+            });
             return
-        } 
-        if (checkemail){
-            res.status(409).send({error: "the Email is already taken"});
+        }
+        if (checkemail) {
+            res.status(409).send({
+                error: "the Email is already taken"
+            });
             return
         }
         if (checkusername == null && checkemail == null) {
@@ -64,50 +72,71 @@ app.post('/create-account', async (req, res) => {
                 "lastname": req.body.lastname,
                 "password": req.body.password,
             };
-    
+
             let insertuser = await data.insertOne(newuser);
-            res.send(await data.distinct("_id", {"username": newuser.username}));
-            res.status(200).send({succes: "successfully created new useraccount"});
+            const userId = await data.distinct("_id", {
+                "username": newuser.username
+            })
+            res.status(200).send({
+                succes: "successfully created new useraccount",
+                id: userId
+            });
         }
-        
+
     } catch (error) {
         console.log(error);
-        res.status(406).send(error.message); 
-    } finally{
+        res.status(406).send(error.message);
+    } finally {
         await client.close();
     }
 
 })
 
 app.post('/login', async (req, res) => {
-    if ( !req.body.username || !req.body.password){
-        res.status(401).send({error: "please fill in all fields"});
+    if (!req.body.username || !req.body.password) {
+        res.status(401).send({
+            error: "please fill in all fields"
+        });
         return
     }
 
     try {
         await client.connect();
         data = await client.db().collection("user_data");
-        checkusername = await data.findOne({username: req.body.username});
-        if(checkusername == null) {
+        checkusername = await data.findOne({
+            username: req.body.username
+        });
+        if (checkusername == null) {
             console.log('test 2');
-            res.status(404).send({error: "username or password is wrong"});
+            res.status(404).send({
+                error: "username or password is wrong"
+            });
             return
-        } else{
-            const userdata = await data.findOne({username: req.body.username});
+        } else {
+            const userdata = await data.findOne({
+                username: req.body.username
+            });
             if (userdata.password == req.body.password) {
-                const userId = userdata._id.toString();
-                res.send(await data.distinct("_id", {"username": req.body.username}));
-                console.log(userId);
-            } else{
-                res.status(401).send({error: 'username or password is wrong'})
+                const userId = await data.distinct("_id", {
+                    "username": req.body.username
+                })
+                res.status(200).send({
+                    succes: "successfully logged in",
+                    id: userId
+                });
+            } else {
+                res.status(401).send({
+                    error: 'username or password is wrong'
+                })
             }
-            
+
         }
-    }catch(error){
+    } catch (error) {
         console.log(error);
-        res.status(406).send(error.message);
-    } finally{
+        res.status(406).send({
+            error
+        });
+    } finally {
         await client.close();
     }
 })
@@ -117,4 +146,3 @@ app.post('/login', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`api running at: http://localhost:${PORT}`);
 });
-
